bec1504416c3da747a9af799be6e3368f042e76e
Chris Davila <cdavila7@cnm.edu>
2025-07-13 21:20:16 -0600

    updated packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './index.css'
-import 'flowbite'
+import './index.css';
+import 'lightbox2/dist/css/lightbox.min.css';
+import 'flowbite';
 import {contactForm} from "./contact-form.js";
 import lightbox from 'lightbox2/dist/js/lightbox-plus-jquery';
 
