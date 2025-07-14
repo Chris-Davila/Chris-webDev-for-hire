874dc68fe52b70e0fca47a2a308d6fe3d59ab451
Chris-Davila <intelpentium20_@hotmail.com>
2025-07-13 21:54:36 -0600

    proper reset of packages and important files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './index.css'
+import '/lightbox2/dist/css/lightbox.min.css'
 import 'flowbite'
 import {contactForm} from "./contact-form.js";
-import lightbox from 'lightbox2/dist/js/lightbox-plus-jquery';
 
 contactForm()
 
